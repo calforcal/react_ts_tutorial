b0643e9aebc4fdbab15bc17bf201ddc404490961
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2023-10-24 08:17:11 -0600

    Add TodoList array component

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Todo } from "../model";
+import "./styles.css";
 
-const TodoList = () => {
-  return (
-    <div>TodoList</div>
-  )
+interface Props {
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
+
+const TodoList: React.FC<Props> = ({ todos,setTodos }) => {
+  return <div className='todos'>
+    {todos.map(todo => (
+      <li>{todo.todo}</li>
+    ))}
+  </div>;
+};
 
 export default TodoList
